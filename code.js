e60487c4ba6e48398260be62a4814f468f2bc53c
abdelrahman <abdelrahmanabdellatif0@gmail.com>
2024-05-01 06:03:09 +0300

    Add playRound() function

--- code.js
+++ code.js
@@ -3,7 +3,6 @@
 function computerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let cpuSelection = choices[Math.floor(Math.random() * choices.length)];  
-    console.log(cpuSelection)
     return cpuSelection; 
 }
 
@@ -15,11 +14,11 @@ function humanChoice() {
     humanSelection = prompt("Choose your play!");
     humanSelection = humanSelection.toLowerCase();
     if (humanSelection === "rock" || humanSelection === "paper" || humanSelection === "scissors") {
-        console.log(humanSelection)
         return humanSelection
     }
     else {
         alert(`${humanSelection} is not a valid option!\nPlease enter one of the following options: Rock, Paper, Scissors`)
+        humanChoice()
     }
 
 }
@@ -28,6 +27,8 @@ function humanChoice() {
 // -------------------------------------------------------------------------------------------------------------------------------- //
 // Keep player's scores in a variable that increments based on the winner every round
 
+let humanScore = 0;
+let cpuScore = 0; 
 
 
 
@@ -36,9 +37,50 @@ function humanChoice() {
 // Write playRound() which takes the values from humanChoice and computerChoice as parameters and evaluate the winner of the round thus incrementing
 // score variable by 1
 
+function playRound(humanSelection, cpuSelection) {
 
+    cpuSelection = computerChoice()
+    humanSelection = humanChoice()
 
-
+    if (humanSelection === cpuSelection) {
+        console.log("It's a tie!");
+    }
+    else {
+        switch(humanSelection) {
+            case "rock":
+                if (cpuSelection === "paper"){
+                    console.log("Paper beats Rock, you lose!");
+                    cpuScore++;
+                }
+                else {
+                    console.log("Rock beats Scissors, you win!");
+                    humanScore++;
+                }
+                break;
+            case "paper":
+                if (cpuSelection === "scissors"){
+                    console.log("Scissors beats Paper, you lose!");
+                    cpuScore++;
+                }
+                else {
+                    console.log("Paper beats Rock, you win!");
+                    humanScore++;
+                }
+                break;
+            case "scissors":
+                if (cpuSelection === "rock"){
+                    console.log("Rock beats Scissors, you lose!");
+                    cpuScore++;
+                }
+                else {
+                    console.log("Scissors beats Paper, you win!");
+                    humanScore++;
+                }
+                break;
+                
+        }
+    }
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------- //
 // write playGame() which will call playRound() 5 times keep track of the player's scores and returning the winner of the game at the end

310193f60f42611d8128b7fe67646651c9a00249
abdelrahman <abdelrahmanabdellatif0@gmail.com>
2024-05-13 21:53:07 +0300

    Add functionality to the website + edit playRound()
    User will now be able to play the game using on-screen buttons instead
    of prompts score, round winner, game winner also appear on screen now.

--- code.js
+++ code.js
@@ -1,72 +1,84 @@
+// Keep player's scores in a variable that increments based on the winner every round
+let humanScore = 0;
+let cpuScore = 0; 
+
+const score = document.querySelector("#score")
+score.textContent = `${humanScore} - ${cpuScore}`
+
+const cpuText = document.querySelector("#cputext")
+const announcement = document.querySelector("#roundwinner")
+let atext = ""
+
+const restartBtn = document.querySelector("#restart")
+restartBtn.addEventListener("click", () => {
+    console.log("clicked")
+})
+
+const choiceBtns = document.querySelectorAll(".playerchoice") 
+choiceBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanChoice = button.textContent.toLowerCase()
+        cpuChoice = computerChoice()
+        playRound(humanChoice, cpuChoice)
+    });
+});
 // Write computerChoice() to choose rock paper or scissors randomly and return that value
 
 function computerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    let cpuSelection = choices[Math.floor(Math.random() * choices.length)];  
-    return cpuSelection; 
+    let cpuChoice = choices[Math.floor(Math.random() * choices.length)];  
+    return cpuChoice; 
 }
-// Write humanChoice() which will prompt the user for their choice, make sure the value is case-insensitive and return it
 
-function humanChoice() {
-    humanSelection = prompt("Choose your play!");
-    humanSelection = humanSelection.toLowerCase();
-    if (humanSelection === "rock" || humanSelection === "paper" || humanSelection === "scissors") {
-        return humanSelection
-    }
-    else {
-        alert(`${humanSelection} is not a valid option!\nPlease enter one of the following options: Rock, Paper, Scissors`)
-        humanChoice()
-    }
-
-}
-// Keep player's scores in a variable that increments based on the winner every round
-
-let humanScore = 0;
-let cpuScore = 0; 
 // Write playRound() which takes the values from humanChoice and computerChoice as parameters and evaluate the winner of the round thus incrementing
 // score variable by 1
-function playRound(humanSelection, cpuSelection) {
+function playRound(humanChoice, cpuChoice) {
 
-    cpuSelection = computerChoice()
-    humanSelection = humanChoice()
-
-    if (humanSelection === cpuSelection) {
-        console.log("It's a tie!");
+    if (humanChoice === cpuChoice) {
+        atext = "It's a tie!"
     }
     else {
-        switch(humanSelection) {
+        switch(humanChoice) {
             case "rock":
-                if (cpuSelection === "paper"){
-                    console.log("Paper beats Rock, you lose!");
+                if (cpuChoice === "paper"){
+                    atext = "PAPER beats ROCK, you lose!"
                     cpuScore++;
                 }
                 else {
-                    console.log("Rock beats Scissors, you win!");
+                    atext = "ROCK beats SCISSORS, you win!"
                     humanScore++;
                 }
                 break;
             case "paper":
-                if (cpuSelection === "scissors"){
-                    console.log("Scissors beats Paper, you lose!");
+                if (cpuChoice === "scissors"){
+                    atext = "SCISSORS beats PAPER, you lose!"
                     cpuScore++;
                 }
                 else {
-                    console.log("Paper beats Rock, you win!");
+                    atext = "PAPER beats ROCK, you win!"
                     humanScore++;
                 }
                 break;
             case "scissors":
-                if (cpuSelection === "rock"){
-                    console.log("Rock beats Scissors, you lose!");
+                if (cpuChoice === "rock"){
+                    atext = "ROCK beats SCISSORS, you lose!"
                     cpuScore++;
                 }
                 else {
-                    console.log("Scissors beats Paper, you win!");
+                    atext = "SCISSORS beats PAPER, you win!";
                     humanScore++;
                 }
                 break;
                 
         }
+    }
+
+    score.textContent = `${humanScore} - ${cpuScore}`
+    cpuText.textContent = `CPU Chose ${cpuChoice.toUpperCase()}`
+    announcement.textContent = `${atext}`
+
+    if (humanScore === 5 || cpuScore === 5) {
+        resetGame()
     }
 }
 // write playGame() which will call playRound() 5 times keep track of the player's scores and returning the winner of the game at the end
@@ -86,3 +98,5 @@ function playGame() {
         alert(`Player: ${humanScore}\nCPU: ${cpuScore}\nYou Lose!`)
     }
 }
+
+// Add resetGame()
